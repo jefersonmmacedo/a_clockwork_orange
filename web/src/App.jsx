2d0b98e6d3230478dcc67c9cec472e27688b8996
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2021-09-02 09:09:05 -0300

    Alterando extensões de arquivos e comentando o projeto para etendimento dos membros

---
+++ web/src/App.jsx
@@ -0,0 +1,19 @@
+// Assim que importamos o css
+ import './Global.css';
+
+// Criamos uma função, dentro dela que colocamos tudo que vamos renderizar em tela.
+// Chamamos de componente funcional
+function App() {
+  return (
+    <div className="App">
+     <h1>A Clockwork Orange</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// Dentro a function damos um return()
+// Dentro das () colocamos uma div principal
+// dentro dessa div, vem o HTML
+// Note da div, que temos o atributo className. no html comum colocamos a class CSS, aqui colocamos como className
