05851040471bd4d0fca1a8e7345029d30caa334b
ewasem <eduardowasem@outlook.com>
2021-09-06 10:36:45 -0300

    Criada a tela SplashActivity, foram adicionadas também as fontes e cores do projeto

---
+++ mobile/app/src/main/java/com/squad34/aclockworkorange/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.squad34.aclockworkorange.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squad34.aclockworkorange.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
